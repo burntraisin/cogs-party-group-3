c720e505a9473ed87fa4416a9c8ce78ccd567572
chexedy <106210798+chexedy@users.noreply.github.com>
2025-03-14 20:04:08 -0400

    added script

---
+++ fishing-game/player_1.gd
@@ -0,0 +1,10 @@
+# this script controls the fishing game and updating each players stats
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
