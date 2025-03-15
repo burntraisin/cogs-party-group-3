c0c3adb65141a1f442caeacfb286bbd2d2ef3257
chexedy <106210798+chexedy@users.noreply.github.com>
2025-03-14 20:16:20 -0400

    3/14/2025

---
+++ fishing-game/fish_game.gd
@@ -0,0 +1,12 @@
+#full game controller
+
+extends Node2D
+var plr_count;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Hello");
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
