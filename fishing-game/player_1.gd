83b306bf047fabb42c5f39c657337cabd3c0718e
chexedy <106210798+chexedy@users.noreply.github.com>
2025-04-02 07:44:50 -0400

    Revert "4/1/2025"
    This reverts commit 6d326ce42a182053f5786dc3d6ee5721e1ed0729.

--- fishing-game/player_1.gd
+++ fishing-game/player_1.gd
@@ -1,12 +1,21 @@
 # this script controls the fishing game and updating each players stats
 extends Control
-var current_score = 0;
-const plr_id = 1
 
-signal done_fishing(id)
+var data = LibraryData.new();
+var current_score = 0;
+const plr_id = 1;
+var rarity_sizes = {
+	"CommonBar" = 95.2,
+	"RareBar" = 71.4,
+	"EpicBar" = 47.6,
+	"LegendaryBar" = 23.8,
+}
+
+signal done_fishing(id);
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_on_game_start_fishing(1);
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,4 +41,17 @@ func _on_game_remove_score(id:Variant, score:Variant) -> void:
 	self.Score.text = "[center] Score: " + str(current_score) + " [/center]"
 
 func _on_game_start_fishing(id) -> void:
-	pass;
+	# randomizing the bar order
+	if (id != plr_id):
+		return
+		
+	var bar = self.get_node("InnerProgressBar");
+	var rarity_copy = rarity_sizes.duplicate();
+	var position = 0;
+	
+	for n in 4:
+		var key = rarity_copy.keys().pick_random();
+		bar.get_node(key).position = Vector2(0, position);
+		position += rarity_copy[key];
+		print("Current key: " + key + ". Current position: " + str(position));
+		rarity_copy.erase(key);
