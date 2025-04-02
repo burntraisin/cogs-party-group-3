c899e3a5c40907fb2d1ed2694befbd1b59daabdb
chexedy <106210798+chexedy@users.noreply.github.com>
2025-04-01 20:54:10 -0400

    updated bar rarities

---
+++ fishing-game/library.gd
@@ -0,0 +1,18 @@
+class_name LibraryData extends Node
+
+var fish_score = {
+	"Fish Name" = 25,
+};
+
+var fish_rarity = {
+	"Fish Name" = "Common",
+};
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
