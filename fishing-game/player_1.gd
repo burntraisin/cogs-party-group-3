96f25d68ae5d751f6366f979d3dacaeadef91261
chexedy <106210798+chexedy@users.noreply.github.com>
2025-04-04 13:16:05 -0400

    added arrow

--- fishing-game/player_1.gd
+++ fishing-game/player_1.gd
@@ -5,10 +5,10 @@ var data = LibraryData.new();
 var current_score = 0;
 const plr_id = 1;
 var rarity_sizes = {
-	"CommonBar" = 95.2,
-	"RareBar" = 71.4,
-	"EpicBar" = 47.6,
-	"LegendaryBar" = 23.8,
+	"CommonBar" = float(95.2),
+	"RareBar" = float(71.4),
+	"EpicBar" = float(47.6),
+	"LegendaryBar" = float(23.8),
 }
 
 signal done_fishing(id);
@@ -22,6 +22,11 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func start_tweening():
+	pass
+
+func stop_tweening():
+	pass
 
 func _on_game_add_score(id, score) -> void:
 	if (id != plr_id):
@@ -55,3 +60,5 @@ func _on_game_start_fishing(id) -> void:
 		position += rarity_copy[key];
 		print("Current key: " + key + ". Current position: " + str(position));
 		rarity_copy.erase(key);
+
+	# start the arrow moving
