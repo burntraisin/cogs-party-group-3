08af81c4c5bda508ce372b58132299f4eb9c1873
chexedy <106210798+chexedy@users.noreply.github.com>
2025-04-04 22:12:30 -0400

    added working timer

--- fishing-game/fish_game.gd
+++ fishing-game/fish_game.gd
@@ -2,23 +2,31 @@
 
 extends Node2D
 var plr_count;
-@onready var stats = get_node("Stats");
-@onready var players = get_node("Player");
-
-signal add_score(id, score) 
-signal remove_score(id, score)
-signal start_fishing(id)
-
+var timer_status = 0;
 
 var fish_scene = preload("res://FishGame.tscn");
 
+@onready var stats = get_node("Stats");
+@onready var players = get_node("Player");
+@onready var timer = stats.get_node("Timer").get_node("Name");
+
+signal add_score(id, score);
+signal remove_score(id, score);
+signal start_fishing(id);
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hello");
+	controller();
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if timer_status == 1:
+		var time_left = stats.get_node("TimeRemaining").time_left;
+		var minute = floor(time_left / 60);
+		var second = int(time_left) % 60;
+		var time = "%02d:%02d" % [minute, second];
+		timer.text = "[center] Time Remaining: " + time + " [/center]";
 
 
 func setup(player_data: Array[RefCounted]) -> void:
@@ -28,17 +36,29 @@ func setup(player_data: Array[RefCounted]) -> void:
 		players["Player" + str(i+1)].modulate = player_data[i].color;
 		players["Player" + str(i+1)].show;
 		stats["Player" + str(i+1)].show;
-	
-	
-
-
-func _on_timer_timeout() -> void:
-	pass
-
-	# sorry had to comment this out when testing something else
-	# var fish = fish_scene.instantiate()
-	# fish.position = Vector2(rand_range())
 
 
 func _on_player_1_done_fishing(id: Variant) -> void:
 	pass # Replace with function body.
+
+func controller() -> void:
+	for n in range(10, -0, -1):	
+		print(n);
+		timer.text = "[center] " + str(n) + "... [/center]";
+		await get_tree().create_timer(1).timeout;
+
+	timer.text = "[center] Get Ready... [/center]";
+	await get_tree().create_timer(1).timeout;
+	timer.text = "[center] GO!!! [/center]";
+	await get_tree().create_timer(1).timeout;
+	stats.get_node("TimeRemaining").start();
+	timer_status = 1;
+
+	pass
+
+func _on_time_remaining_timeout():
+	timer_status = 0;
+	timer.text = "[center] Game Over! [/center]";
+	await get_tree().create_timer(3).timeout;
+	timer.text = "[center] And the winner is... [/center]";
+	await get_tree().create_timer(3).timeout;
--- fishing-game/player_1.gd
+++ fishing-game/player_1.gd
@@ -4,18 +4,13 @@ extends Control
 var data = LibraryData.new();
 var current_score = 0;
 const plr_id = 1;
-var rarity_sizes = {
-	"CommonBar" = float(95.2),
-	"RareBar" = float(71.4),
-	"EpicBar" = float(47.6),
-	"LegendaryBar" = float(23.8),
-}
+var rarity = ["CommonBar", "RareBar", "EpicBar", "LegendaryBar"];
 
 signal done_fishing(id);
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	_on_game_start_fishing(1);
+	run_fishing(1);
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -45,20 +40,18 @@ func _on_game_remove_score(id:Variant, score:Variant) -> void:
 	current_score = new_score;
 	self.Score.text = "[center] Score: " + str(current_score) + " [/center]"
 
-func _on_game_start_fishing(id) -> void:
+func run_fishing(id) -> void:
 	# randomizing the bar order
 	if (id != plr_id):
 		return
 		
-	var bar = self.get_node("InnerProgressBar");
-	var rarity_copy = rarity_sizes.duplicate();
-	var position = 0;
-	
-	for n in 4:
-		var key = rarity_copy.keys().pick_random();
-		bar.get_node(key).position = Vector2(0, position);
-		position += rarity_copy[key];
-		print("Current key: " + key + ". Current position: " + str(position));
-		rarity_copy.erase(key);
+	var bar = self.get_node("ProgressBar").get_node("RarityHolder");
+	randomize();
+	rarity.shuffle();
 
+	for n in 4:
+		bar.move_child(bar.get_node(rarity[n]), n);
 	# start the arrow moving
+
+func _on_game_start_fishing(id) -> void:
+	pass
