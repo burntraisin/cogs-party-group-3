5131c038a2702b43759b3c3e21cdbdc538ab82d7
chexedy <106210798+chexedy@users.noreply.github.com>
2025-04-07 20:37:26 -0400

    added main menu

--- fishing-game/main.gd
+++ fishing-game/main.gd
@@ -1,12 +1,10 @@
 # Main Menu controller
 
 extends Node2D
-signal send_data_to_main
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,4 +16,3 @@ func _on_minigame_manager_game_started(player_data_array: Array[RefCounted]) -> 
 	var game = game_scene.instantiate();
 	
 	game.setup(player_data_array);
-	
